60de7a888aec59d1139a6e4e4a1eb768958bccb1
wsilva <wsilva.ekan@iamspe.sp.gov.br>
2024-09-22 05:37:12 -0300

    ajuste

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from Spring Boot!";
+        return "Hello from Spring Booot!";
     }
 }
